2b748b7b565e2ea5f2432fdbf2ecedbf821fc702
RafaelRodriguezLara <35503348+RafaelRodriguezLara@users.noreply.github.com>
2020-07-01 16:27:08 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Start from './components/Start';
+import './App.css';
+import Quiz from './components/Quiz';
+import Router from './Router';
+
+function App() {
+  return (
+    <div className="App">
+      <Router></Router>
+    </div>
+  );
+}
+
+export default App;
