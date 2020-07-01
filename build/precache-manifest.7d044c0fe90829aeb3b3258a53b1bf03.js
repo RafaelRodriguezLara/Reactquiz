01495b8b2da52370a49d1981c99e196b06541121
RafaelRodriguezLara <35503348+RafaelRodriguezLara@users.noreply.github.com>
2020-07-01 16:24:03 +0200

    Add files via upload

---
+++ build/precache-manifest.7d044c0fe90829aeb3b3258a53b1bf03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edc10c69dc076ba698851ac1c35a061c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5846f96ee2ef79a75b7b",
+    "url": "/static/css/main.47f3825d.chunk.css"
+  },
+  {
+    "revision": "74b66f15a72b8d531cd7",
+    "url": "/static/js/2.ca6efe76.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ca6efe76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5846f96ee2ef79a75b7b",
+    "url": "/static/js/main.fadc4ebe.chunk.js"
+  },
+  {
+    "revision": "df7d820ded1d4db0ff91",
+    "url": "/static/js/runtime-main.410ddc6b.js"
+  }
+]);
